d8fa3c9165cd76a6120f12d835fd75933b5f3be9
John P Huckett <john.huckett@deepfriedcircuits.com>
2015-03-06 07:13:39 +0000

    changed input fields to editable divs to display HTML correctly.

--- regexp.js
+++ regexp.js
@@ -1,21 +1,33 @@
+
 window.onload = init();
 	var i;
 	var v;
+	var y;
 	function init(){
 		i ='';
 		x='';
 	}
 	function regExField(){
-		i = document.getElementById("regExInput").value;
-		//console.log("regex " + i);
+		i = document.getElementById("regExInput").innerHTML;
+		console.log("regex " + i);
 		testRegEx();
 	}
 	function codeField(){
-		x = document.getElementById("regExCode").value;
-		//console.log("code field " + x);
+		x = document.getElementById("regExCode").innerHTML;
+		console.log("code field " + x);
 		testRegEx();
 	}
 	function testRegEx(){
-		console.log('i = '+i);
-		console.log('x = '+x);
+		var foo = document.getElementById('regExInput').value;
+		var i = new RegExp(foo,"g");
+		
+		x = document.getElementById('regExCode').innerHTML;
+		//y = document.getElementById('output').value;
+		//document.getElementById('output').innerHTML = x;
+		//y =x;
+		x = x.replace(i, '<span id="highlight">&$</span>' )
+		//y = x.replace(/fish/, '<span id="highlight">&$</span>' )
+		//console.log('i = '+i);
+		//console.log('x = '+x);
+		document.getElementById('regExCode').value=x;
 	}
