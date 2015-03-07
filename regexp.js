515bf7f4aceee683be320cbc8b5cd4d7575c194f
John P Huckett <john.huckett@deepfriedcircuits.com>
2015-03-07 10:59:54 +0000

    derp

--- regexp.js
+++ regexp.js
@@ -9,27 +9,31 @@ window.onload = init();
 	}
 	function regExField(){
 		i = document.getElementById("regExInput").value;
-		console.log("regex " + i);
-		//testRegEx();
+		//console.log("regex " + i);
+		testRegEx();
 	}
 	function codeField(){
 		x = document.getElementById("regExCode").value;
 		document.getElementById("output").innerHTML = x;
+
 	
-		console.log("code field " + x);
-		//testRegEx();
+		//console.log("code field " + x);
+		testRegEx();
 	}
 	function testRegEx(){
+		var modifier = document.getElementById('modifierInput').value;
+		console.log(modifier);
 		var foo = document.getElementById('regExInput').value;
-		var i = new RegExp(foo,"g");
+		var i = new RegExp(foo,modifier);
+		console.log(i);
+		//console.log(i);
+		//x = x.replace(i, '<span id="highlight">&$</span>' )
 		
-		x = document.getElementById('regExCode').innerHTML;
-		//y = document.getElementById('output').value;
-		//document.getElementById('output').innerHTML = x;
-		//y =x;
-		x = x.replace(i, '<span id="highlight">&$</span>' )
-		//y = x.replace(/fish/, '<span id="highlight">&$</span>' )
+		y = x.replace(i, '<span style="background-color:orange">$&</span>' );
+		document.getElementById("output").innerHTML = y;
 		//console.log('i = '+i);
 		//console.log('x = '+x);
+		console.log(y);
 		document.getElementById('regExCode').value=x;
+		
 	}
