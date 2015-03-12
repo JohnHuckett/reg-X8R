58338f230735b31a79706689e350c5cd0577ed3a
John P Huckett <john.huckett@deepfriedcircuits.com>
2015-03-12 08:01:48 +0000

    added modifier error message

--- regexp.js
+++ regexp.js
@@ -1,35 +1,51 @@
+"use strict"
 
 window.onload = init();
 	var i;
-	var v;
-	var y;
+	var strToTest;
+
 	function init(){
 		i ='';
-		x='';
+		strToTest='';
 	}
+
 	function regExField(){
 		i = document.getElementById("regExInput").value;
-		
 		testRegEx();
-		
 	}
+
 	function codeField(){
-		x = document.getElementById("regExCode").value;
-		document.getElementById("output").innerHTML = x;
+		strToTest = document.getElementById("regExCode").value;
+		document.getElementById("output").innerHTML = strToTest;
 		testRegEx();
+	}
+	function checkModifier(modifier){
+		var patt = new RegExp("([a-fhj-ln-z\d\WA-Z])|([\W\w\d]{4})|(g{2}|m{2}|i{2})");
+		var res = patt.test(modifier);
+		if(res==true){
+			document.getElementById('modifierError').innerHTML = "INVALID MODIFIER";
+			return(false);
+		}
+		if(res==false){
+			document.getElementById('modifierError').innerHTML = "";
+			return(true);
+		}
 	}
 	function testRegEx(){
 		var modifier = document.getElementById('modifierInput').value;
-		var regExInput = document.getElementById('regExInput').value;
-		var i = new RegExp(regExInput,modifier);
-		y = x.replace(i, 'rgx8r0pen$&rgx8rcl0se' );
-		y = y.replace(/&/g,'&amp;');
-		y = y.replace(/</g,'&lt;').replace(/>/g,'&gt;');
-		y = y.replace(/rgx8r0pen/g,'<span id="regExHigh">').replace(/rgx8rcl0se/g,'</span>');
-		document.getElementById("output").innerHTML = y;
-		document.getElementById('regExCode').value=x;
-		document.getElementById('error').innerHTML = '';	
+		if(checkModifier(modifier)==true){
+			var regExInput = document.getElementById('regExInput').value;
+			var i = new RegExp(regExInput,modifier);
+			var y = strToTest.replace(i, 'rgx8r0pen$&rgx8rcl0se' );
+			y = y.replace(/&/g,'&amp;');
+			y = y.replace(/</g,'&lt;').replace(/>/g,'&gt;');
+			y = y.replace(/rgx8r0pen/g,'<span id="regExHigh">').replace(/rgx8rcl0se/g,'</span>');
+			document.getElementById("output").innerHTML = y;
+			document.getElementById('regExCode').value = strToTest;
+			document.getElementById('error').innerHTML = '';
+		}	
 	}
+
 	function showKey(){
 		var foo = document.getElementById('helpButton').innerHTML;
 		if(foo=='HELP'){
@@ -40,7 +56,6 @@ window.onload = init();
 			document.getElementById('sidebar').style.display = 'none';
 			document.getElementById('helpButton').innerHTML = 'HELP';
 		}
-		
 	}
 	
 	window.onerror = function() {
